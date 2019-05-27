37f27906153764be4ad1acfccf1d67516abef8a7
DaejunBae <dpudpu11@gmail.com>
2019-05-27 14:55:17 +0900

    refactor: PointsGenerator.class REGEX Pattern 캐싱 추가

--- src/main/java/coordinate/domain/generator/PointsGenerator.java
+++ src/main/java/coordinate/domain/generator/PointsGenerator.java
@@ -6,15 +6,17 @@ import coordinate.domain.Points;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class PointsGenerator {
     private static final String REGEX = "(\\([0-9]{1,2},[0-9]{1,2}\\)-)+\\([0-9]{1,2},[0-9]{1,2}\\)";
     private static final String DELIMITER = "-";
+    private static final Pattern VALID_INPUT = Pattern.compile(REGEX);
 
     private final String[] coordinates;
 
     public PointsGenerator(String coordinates) {
-        if (!coordinates.matches(REGEX)) {
+        if (!VALID_INPUT.matcher(coordinates).matches()) {
             throw new IllegalArgumentException("유효한 입력이 아닙니다. ex) (10,10)-(10,10)-(20,8)");
         }
         this.coordinates = Arrays.stream(coordinates.split(DELIMITER))
@@ -27,7 +29,7 @@ public class PointsGenerator {
         for (String coordinate : coordinates) {
             int index = coordinate.indexOf(",");
             int x = Integer.parseInt(coordinate.substring(0, index));
-            int y = Integer.parseInt(coordinate.substring(index+1));
+            int y = Integer.parseInt(coordinate.substring(index + 1));
             points.add(new Point(x, y));
         }
         return Points.of(points);
